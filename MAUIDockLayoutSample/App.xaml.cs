05fe257b2cca043b5b4dddd09ead084ae12e0f6c
alamgeer <alamgeer06@gmail.com>
2022-12-07 20:38:48 +0530

    Initial Commit

---
+++ MAUIDockLayoutSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIDockLayoutSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MAUIDockLayoutSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIDockLayoutSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
